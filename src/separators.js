6f62667805aceef58d286165c290e33467876830
ssccccc <810822600@qq.com>
2016-05-23 20:10:58 +0800

    separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,18 +1,43 @@
 'use strict';
 
-function thousands_separators(num) {
-  var b=parselnt(num).toString();
-  var len=b.lenth,r=len%3;
-  if(len<=3)
-    {return b;}
-  else
-  {return b.slice(0,r)+","+b.slice(r,len).match(/\d{3}/g).jion(",");}
+function thousands_separators(str)
+{var newStr = "";
+var count = 0;
 
+if(str.indexOf(".")==-1)
+{
+   for(var i=str.length-1;i>=0;i--)
+   {
+ 	if(count % 3 == 0 && count != 0)
+	{
+   		newStr = str.charAt(i) + "," + newStr;
+    }
+   else
+   {
+   		newStr = str.charAt(i) + newStr;
+   }
+    count++;
+  }
+   
 }
+else
+{
+   for(var i = str.indexOf(".")-1;i>=0;i--){
+ if(count % 3 == 0 && count != 0){
+   newStr = str.charAt(i) + "," + newStr;
+ }else{
+   newStr = str.charAt(i) + newStr; 
+ }
+ count++;
+   }
+   str = newStr + (str + "00").substr((str + "00").indexOf("."),3);
+  
+ }
+}
+
 
 alert (thousands_separators(100));
 alert (thousands_separators(1000));
 alert(thousanda_separators(1000000));
 alert(thousands_separators(1000.0));
 alert(thousands_separators(100.2342));
-
