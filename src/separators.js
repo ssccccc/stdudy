50ce2a260e38c3dae96010838cd73a4de18d61bd
ssccccc <810822600@qq.com>
2016-05-23 19:51:06 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,18 @@
 'use strict';
 
 function thousands_separators(num) {
-  add
+  var b=parselnt(num).toString();
+  var len=b.lenth,r=len%3;
+  if(len<=3)
+    {return b;}
+  else
+  {return b.slice(0,r)+","+b.slice(r,len).match(/\d{3}/g).jion(",");}
+
 }
 
-module.exports = thousands_separators;
+alert (thousands_separators(100));
+alert (thousands_separators(1000));
+alert(thousanda_separators(1000000));
+alert(thousands_separators(1000.0));
+alert(thousands_separators(100.2342));
+
