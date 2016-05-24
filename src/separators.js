5d0256317d1894df4883d59234fd199a8c68821d
ssccccc <810822600@qq.com>
2016-05-24 22:32:19 +0800

    js

--- src/separators.js
+++ src/separators.js
@@ -1,8 +1,9 @@
-<script>
+
 'use strict';
 
-function thousands_separators(str)
-{var newStr = "";
+function thousands_separators(num)
+{var str=num.toString();
+var newStr = "";
 var count = 0;
 
 if(str.indexOf(".")==-1)
@@ -12,34 +13,35 @@ if(str.indexOf(".")==-1)
  	if(count % 3 == 0 && count != 0)
 	{
    		newStr = str.charAt(i) + "," + newStr;
-    }
-   else
-   {
+        }
+      else
+        {
    		newStr = str.charAt(i) + newStr;
-   }
+        }
     count++;
   }
    
 }
 else
 {
-   for(var i = str.indexOf(".")-1;i>=0;i--){
- if(count % 3 == 0 && count != 0){
-   newStr = str.charAt(i) + "," + newStr;
- }else{
-   newStr = str.charAt(i) + newStr; 
- }
- count++;
-   }
+    for(var i = str.indexOf(".")-1;i>=0;i--)
+  {
+     if(count % 3 == 0 && count != 0)
+     {
+   	newStr = str.charAt(i) + "," + newStr;
+     }
+    else
+     {
+   	newStr = str.charAt(i) + newStr; 
+     }
+ 	count++;
+  }
    str = newStr + (str + "00").substr((str + "00").indexOf("."),3);
   
- }
+}
+	
 }
 
 
-alert(thousands_separators(100));
-alert(thousands_separators(1000));
-alert(thousanda_separators(1000000));
-alert(thousands_separators(1000.0));
-alert(thousands_separators(100.2342));
-</script>
+
+
